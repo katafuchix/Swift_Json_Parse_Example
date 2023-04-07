5a7ad80fc918fcaeb391fb2ee4c2b7c3af8b90c6
katafuchix <katafuchix@gmail.com>
2023-04-08 02:34:02 +0900

    Initial Commit

---
+++ Swift_Json_Parse_Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swift_Json_Parse_Example
+//
+//  Created by cano on 2023/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_Json_Parse_Example/Swift_Json_Parse_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Json_Parse_ExampleApp.swift
+//  Swift_Json_Parse_Example
+//
+//  Created by cano on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Json_Parse_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
